a633be4b7be2b4b33d25710f0fc50de9355b3246
Gatopop24 <gatopop25@gmail.com>
2026-09-21 22:07:12 -0400

    Now the enemies have an attack cooldown

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,27 +1,47 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] protected int damage = 10;
-    [SerializeField] protected float damageCooldown; 
+    [SerializeField] protected float damageCooldown = 2; 
     [SerializeField] protected float currentCooldown;
+    [SerializeField] protected Health health;
+    [SerializeField] protected bool canAttack = true;
 
     protected virtual void Start()
     {
-        //currentCooldown = damageCooldown; for future cooldown
+        currentCooldown = damageCooldown;
+    }
+
+    protected virtual void Update()
+    {
+        if(currentCooldown > 0f)
+        {
+            currentCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            canAttack = true;
+        }
     }
 
     protected virtual void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            Health health = collision.gameObject.GetComponent<Health>();
-            DealDamage(health);
+            health = collision.gameObject.GetComponent<Health>();
+            if(canAttack)
+            {
+                DealDamage(health);
+            }
         }
     }
 
     protected virtual void DealDamage(Health health)
     {
         health.TakeDamage(damage);
+        canAttack = false;
+        currentCooldown = damageCooldown;
     }
 }
