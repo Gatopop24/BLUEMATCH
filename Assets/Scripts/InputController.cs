67f159cd84426599807b75529b746049daf1300e
Gatopop24 <gatopop25@gmail.com>
2026-09-22 22:36:44 -0400

    Simple map inspired on redmatch. Guns now can reload and hace limited ammo

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -44,5 +44,6 @@ public class InputController : MonoBehaviour
         public const int LeftHook = 8;
         public const int Fire = 9;
         public const int ChangeGun = 10;
+        public const int Reload = 11;
     }
 }
